6c82a2aa5251f580de806305c64033fc4afcd9c4
theoneand33 <120695243+theoneand33@users.noreply.github.com>
2026-06-03 10:33:35 +1200

    add sh script

---
+++ magic.sh
@@ -0,0 +1,12 @@
+curl https://downloads.arduino.cc/arduino-ide/arduino-ide_2.3.9_Linux_64bit.AppImage --output ~/arduino.appimage
+chmod +x ~/arduino.appimage
+sudo apt install -y libfuse2t64
+rm -f "$HOME/.local/share/applications/arduino.desktop"
+cat <<EOF > "$HOME/.local/share/applications/arduino.desktop"
+[Desktop Entry]
+Type=Application
+Name=Arduino IDE
+Exec=~/arduino.appimage
+Terminal=false
+Categories=Utility;
+EOF
